0722588e13812465b82b4abd7ff5d1f79ab1424b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-03 22:43:11 +0100

    Initial commit

---
+++ .scripts/serve-app.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Placeholder.
+#
+# Install and configure a working app:
+# - CakePHP 2.x or 3.x
+# - CakePHP default app or FriendsOfCake app-template
+# - nginx website
+# - MySQL database
+
+composer --prefer-dist -sdev create-project friendsofcake/app-template /home/vagrant/Apps/cake3 dev-cake3
