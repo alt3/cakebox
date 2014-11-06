248028e5b58e05c7f8d590993ed1d5e2997d0f8e
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-06 01:09:28 +0100

    Fixes printf output

--- .scripts/serve-database.sh
+++ .scripts/serve-database.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-
 # Placeholders for optional username and password command arguments
 DB_USER=user
 DB_PASS=password
 
+# Define script usage
+read -r -d '' USAGE <<-'EOF'
+Creates database and test database with '_test' suffix.
+
+Usage: serve-database [NAME]
+
+  NAME: name to be used for the databases.
+EOF
+
 # Check for required parameter
 if [ -z "$1" ]
   then
-  	echo "Error: missing required parameter."
-    echo "Usage: "
-    echo " serve-database name"
+    printf "\n$USAGE\n\nError: missing required parameter.\n\n"
     exit 1
 fi
 
@@ -21,7 +27,7 @@ if [[ $1 == *.* ]]
     exit 1
 fi
 
-# Display feedback during Vagrant provisioning
+# Vagrant provisioning feedback
 echo "Creating databases for $1"
 
 # Create databases unless they already exist
