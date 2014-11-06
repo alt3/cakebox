86fb807892004ef84afc4ba47a2f3043d0f9c306
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-06 09:39:23 +0100

    Composer installs using vagrant user

--- .scripts/serve-app.sh
+++ .scripts/serve-app.sh
@@ -15,21 +15,20 @@ APP_NAME=`echo $1 | sed 's/\.[^.]*$//'`
 
 # Vagrant provisioning feedback
 echo "Creating Cake app $1"
-if [ -d "$APP_DIR/$APP_NAME" ]
+if [ -d "$APP_DIR/$1" ]
   then
-    echo "$APP_DIR/$APP_NAME is not empty."
+    echo "$APP_DIR/$1 is not empty."
     exit 1
 fi
 
 # Generate the Nginx site configuration file (after we are sure the directory is present)
-sudo -s source /cakebox/serve-site.sh $1 $APP_DIR/$APP_NAME
+source /cakebox/serve-site.sh $1 $APP_DIR/$1
 
 # Create the MySQL database (unless it exists)
 source /cakebox/serve-database.sh $APP_NAME
 
 # Install CakePHP 2 application using the FriendsOfCake app-template
-composer --prefer-dist -sdev create-project friendsofcake/app-template /home/vagrant/Apps/$1
-
-
+echo "Composer installing FoC Cake2 app-template into $APP_DIR/$1"
+su vagrant -c "composer --prefer-dist --dev create-project friendsofcake/app-template $APP_DIR/$1"
 
 # Create .env file
