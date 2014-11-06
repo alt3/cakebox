ccc8bd2a546b55711eb8e5ce3f6947376af52598
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-06 01:11:56 +0100

    Standardizes construction

--- .scripts/serve-app.sh
+++ .scripts/serve-app.sh
@@ -10,18 +10,26 @@ if [ -z "$1" ]
 fi
 
 # Generate convenience variable
+APP_DIR=/home/vagrant/Apps
 APP_NAME=`echo $1 | sed 's/\.[^.]*$//'`
-echo "NAME = $APP_NAME"
 
-# Generate the Nginx site configuration file
-sudo -s source /cakebox/serve-site.sh $1/app/webroot /home/vagrant/Apps/$APP_NAME
+# Vagrant provisioning feedback
+echo "Creating Cake app $1"
+if [ -d "$APP_DIR/$APP_NAME" ]
+  then
+    echo "$APP_DIR/$APP_NAME is not empty."
+    exit 1
+fi
+
+# Generate the Nginx site configuration file (after we are sure the directory is present)
+sudo -s source /cakebox/serve-site.sh $1 $APP_DIR/$APP_NAME
 
 # Create the MySQL database (unless it exists)
 source /cakebox/serve-database.sh $APP_NAME
 
 # Install CakePHP 2 application using the FriendsOfCake app-template
-#
-# @TODO: UNLESS DIRECTORY ALREADY EXISTS
 composer --prefer-dist -sdev create-project friendsofcake/app-template /home/vagrant/Apps/$1
+
+
 
 # Create .env file
