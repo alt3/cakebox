635e1f8ab9f3d9511f525e3603242b2168f26f1d
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-06 10:25:58 +0100

    Check for empty dir instead of exist

--- .scripts/serve-app.sh
+++ .scripts/serve-app.sh
@@ -15,11 +15,6 @@ APP_NAME=`echo $1 | sed 's/\.[^.]*$//'`
 
 # Vagrant provisioning feedback
 echo "Creating Cake app $1"
-if [ -d "$APP_DIR/$1" ]
-  then
-    echo "$APP_DIR/$1 is not empty."
-    exit 1
-fi
 
 # Generate the Nginx site configuration file (after we are sure the directory is present)
 source /cakebox/serve-site.sh $1 $APP_DIR/$1
@@ -27,8 +22,14 @@ source /cakebox/serve-site.sh $1 $APP_DIR/$1
 # Create the MySQL database (unless it exists)
 source /cakebox/serve-database.sh $APP_NAME
 
-# Install CakePHP 2 application using the FriendsOfCake app-template
-echo "Composer installing FoC Cake2 app-template into $APP_DIR/$1"
+# Install CakePHP 2 application using FriendsOfCake app-template
+echo "Composer installing FriendsOfCake app-template"
+#if [ "$(ls -A $APP_DIR/$1)" ]
+if [ "find $APP_DIR/$1 -depth -type d -empty" ]
+  then
+    echo " * Skipping: $APP_DIR/$1 not empty"
+    exit 0
+fi
 su vagrant -c "composer --prefer-dist --dev create-project friendsofcake/app-template $APP_DIR/$1"
 
 # Create .env file
