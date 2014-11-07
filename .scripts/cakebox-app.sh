7b29e8339eab1b8835987ae82b2919818b0b5415
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-07 18:24:15 +0100

    Isolates friendsofcake2 installer

--- .scripts/cakebox-app.sh
+++ .scripts/cakebox-app.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # Define script usage
 read -r -d '' USAGE <<-'EOF'
 Installs and configures a fully working CakePHP application using
@@ -25,14 +24,13 @@ if [[ "$2" != "cakephp" && "$2" != "friendsofcake" ]]; then
   exit 1
 fi
 
-# Determine CakePHP version to use
+# Export variables to make them available in installer scripts
+export FQDN=$1
+export TEMPLATE=$2
 export CAKE_VERSION=3
 if [[ "$3" && "$3" -eq 2 ]]; then
   export CAKE_VERSION=2
 fi
-
-# export variables shared between app-installer scripts
-export FQDN=$1
 export APPS_ROOT=/home/vagrant/Apps
 export APP_DIR="$APPS_ROOT/$FQDN"
 export SALT="Replace-Insecure-Cakebox-Salt-And-Cipher"
@@ -43,6 +41,7 @@ export DATABASE_PASSWORD="password"
 export TEST_DATABASE=$DATABASE"_test"
 export TEST_DATABASE_USER="user"
 export TEST_DATABASE_PASSWORD="password"
+export INSTALLERS_DIR="/cakebox/app-installers"
 
 # Convenience function to check target directory before running Git and Composer
 # installs. Please note that $1 is the function parameter here and NOT the first
@@ -60,13 +59,14 @@ function dir_available {
 export -f dir_available
 
 # Run app-specific installer script
-INSTALLERS=/cakebox/app-installers
-if [[ "$2" = 'cakephp' && "$CAKE_VERSION" -eq 2 ]]; then
-  $INSTALLERS/cakephp2.sh
-elif [[ "$2" = 'cakephp' && "$CAKE_VERSION" -eq 3 ]]; then
-  $INSTALLERS/cakephp3.sh
+if [[ "$TEMPLATE" = 'cakephp' && "$CAKE_VERSION" -eq 2 ]]; then
+  $INSTALLERS_DIR/cakephp2.sh
+elif [[ "$TEMPLATE" = 'cakephp' && "$CAKE_VERSION" -eq 3 ]]; then
+  $INSTALLERS_DIR/cakephp3.sh
+elif [[ "$TEMPLATE" = 'friendsofcakephp' && "$CAKE_VERSION" -eq 2 ]]; then
+  $INSTALLERS_DIR/friendsofcake2.sh
 else
-  $INSTALLERS/friendsofcake.sh
+  $INSTALLERS_DIR/friendsofcake3.sh
 fi
 
 # Create the MySQL databases
