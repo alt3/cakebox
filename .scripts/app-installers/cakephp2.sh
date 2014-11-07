00f74eb3982b8316c578a40f78a259560d2e7e06
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-07 17:29:24 +0100

    Hardens cakephp2 installer

--- .scripts/app-installers/cakephp2.sh
+++ .scripts/app-installers/cakephp2.sh
@@ -12,8 +12,13 @@ TMP_DIR="$APP_DIR/app/tmp"
 # Provide Vagrant provisioning feedback
 echo "Creating CakePHP $CAKE_VERSION.x application"
 
-# Clone the repository
-su vagrant -c "git clone git://github.com/cakephp/cakephp.git $APP_DIR"
+# Clone the cakephp repository
+if dir_available "$APP_DIR"
+  then
+    su vagrant -c "git clone git://github.com/cakephp/cakephp.git $APP_DIR"
+  else
+    echo " * Skipping Git installation: $APP_DIR not empty"
+fi
 
 # Generate the Nginx site configuration file
 /cakebox/cakebox-site.sh $FQDN $APP_DIR/app/webroot || exit 1
@@ -28,8 +33,14 @@ cp "$CONFIG_DATABASE_SOURCE" "$CONFIG_DATABASE_TARGET"
 sed -i "/'database_name'/c'database' => '$DATABASE'," "$CONFIG_DATABASE_TARGET"
 sed -i "/'test_database_name'/c'database' => '$TEST_DATABASE'," "$CONFIG_DATABASE_TARGET"
 
-# Install CakePHP DebugKit
-su vagrant -c "git clone git://github.com/cakephp/debug_kit.git $PLUGIN_DIR/DebugKit"
+# Install the CakePHP DebugKit
+echo "Installing DebugKit plugin"
+if dir_available "$PLUGIN_DIR/DebugKit"
+  then
+    su vagrant -c "git clone git://github.com/cakephp/debug_kit.git $PLUGIN_DIR/DebugKit"
+  else
+    echo " * Skipping Git installation: $APP_DIR not empty"
+fi
 
 # Enable Debugkit in bootstrap. php
 if [ "$( grep 'loadAll()' $CONFIG_BOOTSTRAP | wc -l )" -ne 2 ]
