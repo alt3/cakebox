159fe3e4ae7ab0395d321ff2614b57e2b25ccea9
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-07 17:47:05 +0100

    Hardens cakephp3 installer

--- .scripts/app-installers/cakephp3.sh
+++ .scripts/app-installers/cakephp3.sh
@@ -4,27 +4,21 @@
 ENV="$APP_DIR/app/Config/.env"
 
 # Provide Vagrant provisioning feedback
-echo "Creating CakePHP $CAKE_VERSION.x application using CakePHP Application Skeleton."
+echo "Creating CakePHP 3.x application"
 
-# Generate the Nginx site configuration file
-/cakebox/cakebox-site.sh $FQDN $APP_DIR/webroot || exit 1
-
-# Install CakePHP 2 application using FriendsOfCake app-template
-echo "Composer installing"
-
-# Only run composer if directory does not exist OR is empty
-if [ -d "$APP_DIR" ]; then
-  DIR_COUNT="$( find $APP_DIR -mindepth 1 -maxdepth 1 | wc -l )"
-fi
-
-if [ $DIR_COUNT ]; then
-    echo " * Skipping: $APP_DIR is not empty"
+# Composer install cake3 using the Application Skeleton
+if dir_available "$APP_DIR"
+  then
+    su vagrant -c "composer create-project --prefer-dist -s dev cakephp/app $APP_DIR"``
   else
-    su vagrant -c "composer create-project --prefer-dist -s dev cakephp/app $APP_DIR"
+    echo " * Skipping Composer installation: $APP_DIR not empty"
 fi
 
-# Update generated app.php configuration file
+# Update auto-generated app.php configuration file
 APP_PHP="$APP_DIR/config/app.php"
 sed -i "/'database'/c'database' => '$DATABASE'," "$APP_PHP"
 sed -i "/'username'/c'username' => '$DATABASE_USER'," "$APP_PHP"
 sed -i "/'password'/c'password' => '$DATABASE_PASSWORD'," "$APP_PHP"
+
+# Generate the Nginx site configuration file
+/cakebox/cakebox-site.sh $FQDN $APP_DIR/webroot || exit 1
