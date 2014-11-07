7a75889a55ccb1e6497e7c5b35f52e21ce63a11e
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-07 23:20:59 +0100

    Adds cakebox-database USERNAME PASSWORD arguments

--- .scripts/cakebox-database.sh
+++ .scripts/cakebox-database.sh
@@ -6,11 +6,13 @@ DB_PASS=password
 
 # Define script usage
 read -r -d '' USAGE <<-'EOF'
-Creates database and test database with '_test' suffix.
+Creates two MySQL databases, one suffixed with '_test'.
 
-Usage: cakebox-database [NAME]
+Usage: cakebox-database [NAME] <[USERNAME]> <[PASSWORD]>
 
-  NAME: name to be used for the databases.
+    NAME      name to be used for the databases
+    USERNAME  name of the user to grant localhost access, defaults to 'user'
+    PASSWORD  password for the user, defaults to 'password'
 EOF
 
 # Check for required parameter
@@ -21,9 +23,24 @@ if [ -z "$1" ]
 fi
 
 # Convenience variables. Please note that database names should not contain
-# dots so we replace them with with underscores.
+# dots so they are replaced  with with underscores. Test credentials are
+# dependent on decent argument parsing now absent so they use the same args.
 DB=`echo $1 | sed 's/\./_/g'`
 DB_TEST=$DB"_test"
+
+if [ "$2" ];then
+    DATABASE_USER=$2
+  else
+    DATABASE_USER="user"
+fi
+
+if [ "$3" ];then
+    DATABASE_PASSWORD=$3
+  else
+    DATABASE_PASSWORD="password"
+fi
+TEST_DATABASE_USER="$DATABASE_USER"
+TEST_DATABASE_PASSWORD="$DATABASE_PASSWORD"
 
 # Vagrant provisioning feedback
 echo "Creating databases for $DB"
@@ -38,5 +55,5 @@ if [ -d "/var/lib/mysql/$DB" ]
 fi
 
 # Always set database permissions
-mysql -uroot -e "GRANT ALL ON \`$DB\`.* to  '$DB_USER'@'localhost' identified by '$DB_PASS'"
-mysql -uroot -e "GRANT ALL ON \`$DB_TEST\`.* to '$DB_USER'@'localhost' identified by '$DB_PASS'"
+mysql -uroot -e "GRANT ALL ON \`$DB\`.* to  '$DATABASE_USER'@'localhost' identified by '$DATABASE_PASSWORD'"
+mysql -uroot -e "GRANT ALL ON \`$DB_TEST\`.* to '$TEST_DATABASE_USER'@'localhost' identified by '$TEST_DATABASE_PASSWORD'"
