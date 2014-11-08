b23bde26c187cf13b9b582684651fda9b6cd01ef
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-08 10:59:51 +0100

    Replaces nxensite

--- .scripts/cakebox-site.sh
+++ .scripts/cakebox-site.sh
@@ -2,27 +2,35 @@
 
 # Define script usage
 read -r -d '' USAGE <<-'EOF'
-Creates an Nginx site configuration file.
+Creates a new Nginx site by generating a configuration file, creating the
+related symbolic link in /sites-enabled and reloading nginx.
 
 Usage: cakebox-site [URL] [ROOT]
 
-    URL:  URL used to expose the site
-    ROOT: full path to site root (e.g. /var/www/app.dev/app/webroot)
+    URL   Fully qualified domain name (FQDN) used to expose the site
+    ROOT  full path to site webroot (e.g. /var/www/app.dev/app/webroot)
 EOF
 
 # Check required parameters
-if [[ -z "$1" || -z "$2" ]]
-  then
+if [[ -z "$1" || -z "$2" ]]; then
     printf "\n$USAGE\n\nError: missing required parameter.\n\n"
     exit 1
+fi
+
+# Prevent overwriting Cakebox default site
+if [ "$1" = "default" ]; then
+  printf "\n$USAGE\n\nError: editing default Cakebox site is not supported.\n\n"
+  exit 1
 fi
 
 # Convenience variables
 URL=$1
 ROOT=$2
+SITES_AVAILABLE=/etc/nginx/sites-available
+SITES_ENABLED=/etc/nginx/sites-enabled
 
 # Vagrant provisioning feedback
-echo "Creating Nginx site configuration file for $URL"
+echo "Generating Nginx site configuration file for $URL"
 
 # Generate Nginx site configuration file
 block="
@@ -56,13 +64,19 @@ server {
 }
 "
 
-# Create Nginx site configuration file
-echo "$block" > "/etc/nginx/sites-available/$URL"
+# Always create the Nginx site configuration so the command can be re-run
+# (e.g. to fix an incorrectly passed ROOT directory).
+echo "$block" > "$SITES_AVAILABLE/$URL"
 
-# Use nxesnite to:
-# - validate file syntax
-# - create the symbolic link in /etc/nginx/sites-enabled
-nxensite $URL
+# Create a symbolic link in /etc/nginx/sites-enabled. Please note that
+# we do NOT use nxensite so console output stays consistent with other
+# cakebox-script output.
+if [ ! -L "$SITES_ENABLED/$URL" ]; then
+  echo "Enabling site"
+  ln -s "$SITES_AVAILABLE/$URL" "$SITES_ENABLED/$URL"
+else
+  echo " * Site already enabled"
+fi
 
-# Reload Nginx to enable new site
+# Reload Nginx to enable the new site
 service nginx reload
