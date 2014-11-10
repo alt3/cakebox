8e5dc84a43fe4b14ccfe08db3571903414ece2fc
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-10 19:57:14 +0100

    Deletes orphaned files

--- .scripts/commands/installers/cakephp2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    # Sanity checking variable availability after include
-    echo "parent webroot variable still present and is: $webroot!\n";
-
-    # Sanity checking function availability after include
-    if (isValidVersion('3')){
-        echo "3 still validates as a valid CakePHP major version\n";
-    }
