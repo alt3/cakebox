e951bb4f86f6f1b03f11b7329c8ecea3ef55761e
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-23 01:06:37 +0100

    Improves feedback

---
+++ .cakebox/box-info.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+IP=$1
+
+echo " "
+printf "Your Cakebox is ready and waiting at http://%s\n" "$IP"
+echo " "
+echo "Don't forget to update your hosts file and make sure to"
+echo "experiment with the cakebox commands. Happy baking!"
