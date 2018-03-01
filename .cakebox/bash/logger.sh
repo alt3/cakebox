e6f674a67303a17645805599cc1d4988ff282122
Richard Sacco <richardsacco@icloud.com>
2018-02-28 22:21:43 -0800

    Changed Logger
    now script properly creates logs in LogStash format

--- .cakebox/bash/logger.sh
+++ .cakebox/bash/logger.sh
@@ -2,50 +2,74 @@
 
 SCRIPT_LOG=/var/log/cakephp/cakebox.cli.log
 
+## Given a timestamp, message, and tag generate a LogStash Log
+function generate_log() {
+    declare -A logArray
+    logArray[timestamp]="\"$1\""
+    logArray[source]='"cakebox"'
+    logArray[fields]="{\"channel\":\"cli.cakebox\",\"level\":100,\"ctxt_scope\":[]}"
+    logArray[message]="\"$2\""
+    logArray[tags]="[\"cli.cakebox\", \"$3\"]"
+    logArray[type]='"cakephp"'
+
+    printf '{'
+    for lkey in "${!logArray[@]}"
+    do
+        printf '"@%s":%s,' "$lkey" "${logArray[$lkey]}"
+    done
+    echo '}'
+
+}
+
+
 function SCRIPTENTRY(){
- timeAndDate=`date`
+ timestamp=$( date -u +"%Y-%m-%dT%H:%M:%S.%3N%:z" )
  script_name=`basename "$0"`
  script_name="${script_name%.*}"
- echo "[$timeAndDate] [DEBUG]  > $script_name $FUNCNAME" >> $SCRIPT_LOG
+ message="started $script_name"
+ generate_log "$timestamp" "$message" 'debug' >> $SCRIPT_LOG
 }
 
 function SCRIPTEXIT(){
  script_name=`basename "$0"`
  script_name="${script_name%.*}"
- echo "[$timeAndDate] [DEBUG]  < $script_name $FUNCNAME" >> $SCRIPT_LOG
+ message="exited $script_name"
+ generate_log "$timestamp" "$message" 'debug' >> $SCRIPT_LOG
 }
 
 function ENTRY(){
  local cfn="${FUNCNAME[1]}"
- timeAndDate=`date`
- echo "[$timeAndDate] [DEBUG]  > $cfn $FUNCNAME" >> $SCRIPT_LOG
+ timestamp=$( date -u +"%Y-%m-%dT%H:%M:%S.%3N%:z" )
+ message="$cfn $FUNCNAME"
+ generate_log "$timestamp" "$message" 'debug' >> $SCRIPT_LOG
 }
 
 function EXIT(){
  local cfn="${FUNCNAME[1]}"
- timeAndDate=`date`
- echo "[$timeAndDate] [DEBUG]  < $cfn $FUNCNAME" >> $SCRIPT_LOG
+ timestamp=$( date -u +"%Y-%m-%dT%H:%M:%S.%3N%:z" )
+ message="$cfn $FUNCNAME"
+ generate_log "$timestamp" "$message" 'debug' >> $SCRIPT_LOG
 }
 
 
 function INFO(){
  local function_name="${FUNCNAME[1]}"
-    local msg="$1"
-    timeAndDate=`date`
-    echo "[$timeAndDate] [INFO]  $msg" >> $SCRIPT_LOG
+    local message="$1"
+    timestamp=$( date -u +"%Y-%m-%dT%H:%M:%S.%3N%:z" )
+    generate_log "$timestamp" "$message" 'info' >> $SCRIPT_LOG
 }
 
 
 function DEBUG(){
  local function_name="${FUNCNAME[1]}"
-    local msg="$1"
-    timeAndDate=`date`
- echo "[$timeAndDate] [DEBUG]  $msg" >> $SCRIPT_LOG
+    local message="$1"
+    timestamp=$( date -u +"%Y-%m-%dT%H:%M:%S.%3N%:z" )
+    generate_log "$timestamp" "$message" 'debug'  >> $SCRIPT_LOG
 }
 
 function ERROR(){
  local function_name="${FUNCNAME[1]}"
-    local msg="$1"
-    timeAndDate=`date`
-    echo "[$timeAndDate] [ERROR]  $msg" >> $SCRIPT_LOG
+    local message="$1"
+    timestamp=$( date -u +"%Y-%m-%dT%H:%M:%S.%3N%:z" )
+    generate_log "$timestamp" "$message" 'error' >> $SCRIPT_LOG
 }
